0558b22221bf077cb48b7de3be77b33633c6ad3e
JUCILY117 <111986929+JUCILY117@users.noreply.github.com>
2024-12-03 22:31:33 +0530

    fix

--- pufferpanel.sh
+++ pufferpanel.sh
@@ -3,7 +3,7 @@
 curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
 sudo apt-get install pufferpanel
 sudo systemctl enable pufferpanel
-sudo pufferpanel user add --email aniketclass11b2@gmail.com --name aayumats --password ayesha17 --admin
+sudo pufferpanel user add --email admin@gmail.com --name aayumats --password admin --admin
 sudo systemctl enable --now pufferpanel
 
 # I uses https://docs.pufferpanel.com/en/2.x/installing.html to make this easy script, please look over there is there is any problem
